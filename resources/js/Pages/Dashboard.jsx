3f847fd7f7a72d418362e31bbe0108389a9a1070
Tse_Song <tsesangtenzin123@gmail.com>
2024-11-19 16:46:22 +0530

    first commit of the project - laravel,react,inertia,shadCN,breeze

---
+++ resources/js/Pages/Dashboard.jsx
@@ -0,0 +1,30 @@
+import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
+import { Head } from "@inertiajs/react";
+import { Button } from "@/components/ui/button";
+
+export default function Dashboard() {
+    return (
+        <AuthenticatedLayout
+            header={
+                <h2 className="text-xl font-semibold leading-tight text-gray-800">
+                    Dashboard
+                </h2>
+            }
+        >
+            <Head title="Dashboard" />
+
+            <div className="py-12">
+                <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
+                    <div className="overflow-hidden bg-white shadow-sm sm:rounded-lg">
+                        <div className="p-6 text-gray-900">
+                            You're logged in!
+                            <div className="mt-3">
+                                <Button variant="destructive">Button</Button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </AuthenticatedLayout>
+    );
+}
